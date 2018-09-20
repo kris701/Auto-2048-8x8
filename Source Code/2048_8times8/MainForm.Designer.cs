81868f984eb674dd1980c640ada832988b039437
Kristian Skov <kris701kj@gmail.com>
2018-09-20 17:19:04 +0200

    Initial Version

---
+++ Source Code/2048_8times8/MainForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace _2048_8times8
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.StatisticLabel = new System.Windows.Forms.Label();
+            this.SecondTimer = new System.Windows.Forms.Timer(this.components);
+            this.MoveDelayNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.MoveDelayNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(217)))));
+            this.StartButton.FlatAppearance.BorderSize = 0;
+            this.StartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.StartButton.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.ForeColor = System.Drawing.Color.White;
+            this.StartButton.Location = new System.Drawing.Point(12, 12);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Font = new System.Drawing.Font("Lucida Console", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLabel.ForeColor = System.Drawing.Color.White;
+            this.StatusLabel.Location = new System.Drawing.Point(101, 12);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(94, 21);
+            this.StatusLabel.TabIndex = 1;
+            this.StatusLabel.Text = "Stoped";
+            this.StatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // StatisticLabel
+            // 
+            this.StatisticLabel.AutoSize = true;
+            this.StatisticLabel.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatisticLabel.ForeColor = System.Drawing.Color.White;
+            this.StatisticLabel.Location = new System.Drawing.Point(101, 61);
+            this.StatisticLabel.Name = "StatisticLabel";
+            this.StatisticLabel.Size = new System.Drawing.Size(335, 13);
+            this.StatisticLabel.TabIndex = 2;
+            this.StatisticLabel.Text = "Moves: 0 Time elapsed: 0 sec Moves/sec: 0";
+            // 
+            // SecondTimer
+            // 
+            this.SecondTimer.Interval = 1000;
+            this.SecondTimer.Tick += new System.EventHandler(this.SecondTimer_Tick);
+            // 
+            // MoveDelayNumericUpDown
+            // 
+            this.MoveDelayNumericUpDown.BackColor = System.Drawing.Color.DarkGray;
+            this.MoveDelayNumericUpDown.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MoveDelayNumericUpDown.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MoveDelayNumericUpDown.ForeColor = System.Drawing.Color.White;
+            this.MoveDelayNumericUpDown.Location = new System.Drawing.Point(12, 63);
+            this.MoveDelayNumericUpDown.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.MoveDelayNumericUpDown.Name = "MoveDelayNumericUpDown";
+            this.MoveDelayNumericUpDown.Size = new System.Drawing.Size(75, 14);
+            this.MoveDelayNumericUpDown.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(5, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 11);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Delay pr set";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(519, 95);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.StatisticLabel);
+            this.Controls.Add(this.MoveDelayNumericUpDown);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Auto 2048 8*8";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.MoveDelayNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Label StatisticLabel;
+        private System.Windows.Forms.Timer SecondTimer;
+        private System.Windows.Forms.NumericUpDown MoveDelayNumericUpDown;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Source Code/2048_8times8/MainForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Drawing;
+using NDX_Base;
+
+namespace _2048_8times8
+{
+    public partial class MainForm : Form
+    {
+        NDX CustomDesign;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            CustomDesign = new NDX(this);
+            CustomDesign.DragbarBackColor = Color.FromArgb(64, 64, 64);
+            CustomDesign._ButtonBackColor = Color.FromArgb(0, 122, 217);
+            CustomDesign.MaxButtonBackColor = Color.FromArgb(0, 122, 217);
+            CustomDesign.ParentFormDragStyle = NDXDragStyle.Opacity;
+            CustomDesign.Sizable = false;
+            CustomDesign.AutoScroll = false;
+            CustomDesign.HasMaxButton = false;
+            CustomDesign.DoubleClickToMaximize = false;
+            CustomDesign.InitializeNDX();
+        }
+
+        int SecondsSinceStart = 0;
+        int MovesSinceStart = 0;
+        int MovesPrSecCounter = 0;
+        int MovesPrSec = 0;
+        bool Continue = true;
+        string[] MoveSet = { "{LEFT}", "{DOWN}" , "{RIGHT}", "{DOWN}" };
+
+        private async void StartButton_Click(object sender, EventArgs e)
+        {
+            Continue = true;
+            await Run();
+        }
+
+        async Task Run()
+        {
+            int MoveDelay = (int)MoveDelayNumericUpDown.Value;
+            for (int i = 5; i > 0; i--)
+            {
+                StatusLabel.Text = "Starting in " + i + " sec";
+                await Task.Delay(1000);
+            }
+            StatusLabel.Text = "Running, press shift to stop";
+            SecondTimer.Start();
+            while (Continue)
+            {
+                foreach(string Key in MoveSet)
+                {
+                    if (Control.ModifierKeys == Keys.Shift)
+                    {
+                        StatisticLabel.Text = "Moves: " + MovesSinceStart + " Time elapsed: " + SecondsSinceStart + " sec Moves/sec: " + MovesPrSec;
+                        Continue = false;
+                        break;
+                    }
+                    SendKeys.Send(Key);
+                    MovesSinceStart++;
+                    MovesPrSecCounter++;
+                    if (MoveDelay != 0)
+                        await Task.Delay(MoveDelay);
+                    else
+                        Application.DoEvents();
+                }
+            }
+            SecondTimer.Stop();
+            SecondsSinceStart = 0;
+            MovesSinceStart = 0;
+            StatusLabel.Text = "Stoped";
+        }
+
+        private void SecondTimer_Tick(object sender, EventArgs e)
+        {
+            SecondsSinceStart++;
+            MovesPrSec = MovesPrSecCounter;
+            MovesPrSecCounter = 0;
+            StatisticLabel.Text = "Moves: " + MovesSinceStart + " Time elapsed: " + SecondsSinceStart + " sec Moves/sec: " + MovesPrSec;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Continue = false;
+        }
+    }
+}
